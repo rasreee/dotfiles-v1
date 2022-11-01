23a4102f7cb7fffb3be940ba7a29d049e17a1ab0
Lesley Chang <lesley.hanna98@gmail.com>
2022-11-01 19:19:39 -0400

    git.plugin

--- custom/plugins/git/git.plugin.zsh
+++ custom/plugins/git/git.plugin.zsh
@@ -2,23 +2,6 @@ source $ZSH/plugins/git/git.plugin.zsh
 
 gituser=$GITHUB_USER
 
-alias gi="git init"
-alias git-nuke="rm -rf **/.git(N)" gnk="git-nuke" nkgit="git-nuke"
-
-function git-rebase-backwards() {
-  depth=$1
-  echo "rebasing backwards $depth commits"
-  git rebase -i HEAD~$depth
-}
-alias grbb="git-rebase-backwards"
-
-alias grg="git remote get-url origin" gu="grg"
-alias grmca="git rm -r --cached . && git add --all"
-alias grro="git remote remove origin"
-alias grao="git remote add origin"
-
-alias gs="git status"
-
 alias gca="git commit -v -a -m"
 
 function gcap() {
@@ -26,6 +9,61 @@ function gcap() {
   git commit -v -m "$1"
   git push
 }
+
+function git-create-repo() {
+  local commit_msg="first commit"
+  local name="${1:-$(cwd)}"
+
+  rm -rf **/.git(N)
+  git init
+  echo "# $name" >README.md
+  git add .
+  git commit -m "$commit_msg"
+  git branch -M main
+
+  hub create --private $name
+  git push -u -f origin main
+}
+alias gcr="git-create-repo"
+
+function gcob() {
+  local branch=$1
+  git checkout -b $branch
+}
+
+function git-flush() {
+  local current_branch=$(git_current_branch)
+  local rest_args=("$@")
+  
+  git checkout main
+  git push --set-upstream --force origin "$current_branch"
+  git merge "$current_branch" && git push $rest_args
+  git checkout "$current_branch"
+}
+
+alias gfl="git-flush"
+alias gfp="git fetch --prune origin"
+alias gi="git init"
+
+function gpb() {
+ local branch=${1:-$(git_current_branch)}
+  git push -u origin $branch
+}
+
+function gpf() {
+  git push origin $(git_current_branch) --force
+}
+
+function grbb() {
+  depth=$1
+  echo "rebasing backwards $depth commits"
+  git rebase -i HEAD~$depth
+}
+
+alias grg="git remote get-url origin" gu="grg"
+alias grmca="git rm -r --cached . && git add --all"
+alias grro="git remote remove origin"
+alias grao="git remote add origin"
 
 function grs+() {
   local url=$1
@@ -45,39 +83,6 @@ function grhr() {
   grh "$remote/$branch" --hard
 }
 
-alias gfp="git fetch --prune origin"
-
-function git-create-repo() {
-  local commit_msg="first commit"
-  local name="${1:-$(cwd)}"
-
-  rm -rf **/.git(N)
-  git init
-  echo "# $name" >README.md
-  git add .
-  git commit -m "$commit_msg"
-  git branch -M main
-
-  hub create --private $name
-  git push -u -f origin main
-}
-alias gcr="git-create-repo"
-
-function gpof() {
-  git push origin $(git_current_branch) --force
-}
-
-alias git-version="git --version" gv="git-version"
-
-function git-flush() {
-  local current_branch=$(git_current_branch)
-  local rest_args=("$@")
-  
-  git checkout main
-  git push --set-upstream --force origin "$current_branch"
-  git merge "$current_branch" && git push $rest_args
-  git checkout "$current_branch"
-}
-alias gfl="git-flush"
-
 alias gstsh="git stash"
+
+alias gv="git --version"
