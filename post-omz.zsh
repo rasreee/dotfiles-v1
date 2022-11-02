01f2e92c720af46b762dfd554220e9aeca76b07b
Lesley Chang <lesley.hanna98@gmail.com>
2022-11-02 16:37:58 -0400

    wip

--- post-omz.zsh
+++ post-omz.zsh
@@ -6,15 +6,5 @@ export NVM_DIR="$HOME/.nvm"
 # activates nvm
 [[ -r $NVM_DIR/bash_completion ]] && \. $NVM_DIR/bash_completion
 
-# yarn
-NODE_BINARIES="$HOME/.nvm/versions/node/$(nvm version)/bin"
-YARN_PATH="$NODE_BINARIES/yarn"
-export PATH="$PATH:$YARN_PATH"
-unset YARN_PATH NODE_BINARIES
-
-# tabtab source for packages
-# uninstall by removing these lines
-[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
-
 autoload -Uz compinit
 compinit
--- templates/.zshrc
+++ templates/.zshrc
@@ -1,15 +1,6 @@
-_source() {
-  local file="$1"
-  (( $DEBUG_ZSH )) && echo "\$ $(basename $file)"
-  source "$file"
-}
-
 for file ($ZSH/configs/*.zsh); do
-  _source $file
+  source $file
 done
 
-# OMZ
-_source $ZSH/oh-my-zsh.sh
-
-# Post OMZ
-_source $ZSH/post-omz.zsh
+source $ZSH/oh-my-zsh.sh
+source $ZSH/post-omz.zsh
